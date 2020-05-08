6f43d8b9e1171b979ec59634a5ff1d4fef23887e
reinel215 <reinelencuestas@gmail.com>
2020-05-08 13:54:58 -0400

    listo la parte A. se valida la entrada del archivo

--- src/main/java/ExcelFileUpdateExample1.java
+++ src/main/java/ExcelFileUpdateExample1.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.apache.poi.EncryptedDocumentException;
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
@@ -10,6 +11,7 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook; //para crear un excel en blanco
 
 /**
  * This program illustrates how to update an existing Microsoft Excel document.
@@ -23,10 +25,22 @@ public class ExcelFileUpdateExample1 {
 
 	public static void main(String[] args) {
 		String excelFilePath = "Inventario.xlsx";
-		
+		FileInputStream inputStream;
+
 		try {
-			FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
-			Workbook workbook = WorkbookFactory.create(inputStream);
+			File file = new File(excelFilePath);   // se crea una variable del archivo
+			Workbook workbook;
+			
+			if (file.exists()){  //si el archivo existe se obtienen los datos de alli, y si no se crea uno nuevo
+				inputStream = new FileInputStream(file);
+				workbook = WorkbookFactory.create(inputStream);
+				inputStream.close();
+			}
+			else{
+				workbook = new HSSFWorkbook();
+				workbook.createSheet("creada");// se crea una nueva hoja 
+			}
+
 
 			Sheet sheet = workbook.getSheetAt(0);
 
@@ -58,7 +72,31 @@ public class ExcelFileUpdateExample1 {
 
 			}
 
-			inputStream.close();
+	
+			Iterator<Row> iterator = sheet.iterator(); //creamos el iterador sobre la hoja
+         
+       	 while (iterator.hasNext()) {
+            Row nextRow = iterator.next();
+            Iterator<Cell> cellIterator = nextRow.cellIterator(); //y tambien creamos el iterador sobre cada fila de la hoja
+																  //como 2 ciclos anidados
+            while (cellIterator.hasNext()) {
+                Cell cell = cellIterator.next();
+                switch (cell.getCellType()) {   //dependiendo del tipo de datos de la celda hacemos el print correspondiente
+                    case Cell.CELL_TYPE_STRING:
+                        System.out.print(cell.getStringCellValue());
+                        break;
+                    case Cell.CELL_TYPE_BOOLEAN:
+                        System.out.print(cell.getBooleanCellValue());
+                        break;
+                    case Cell.CELL_TYPE_NUMERIC:
+                        System.out.print(cell.getNumericCellValue());
+                        break;
+                }
+                System.out.print(" - ");
+            }
+            System.out.println();
+         }
+			
 
 			FileOutputStream outputStream = new FileOutputStream(excelFilePath);
 			workbook.write(outputStream);
